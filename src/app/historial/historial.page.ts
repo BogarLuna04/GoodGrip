844fa6d650abd0fa34fb15b78fdde4585b3ee9c3
Giovanni <giovanni_mongalla@hotmail.com>
2019-11-27 17:41:19 -0600

    Lista de dias del calendario

--- src/app/historial/historial.page.ts
+++ src/app/historial/historial.page.ts
@@ -9,6 +9,64 @@ import { CalendarComponent } from 'ionic2-calendar/calendar';
 })
 export class HistorialPage implements OnInit {
 
+  lista: Array<any> = [
+    {
+      titulo: "Viernes",
+      description: "Nov-01-2019",
+      redirectTo: "/lista-grips"
+    },
+    {
+      titulo: "Miércoles",
+      description: "Nov-06-2019",
+      redirectTo: "/lista-grips"
+    },
+    {
+      titulo: "Domingo",
+      description: "Nov-10-2019",
+      redirectTo: "/lista-grips"
+    },
+    {
+      titulo: "Martes",
+      description: "Nov-12-2019",
+      redirectTo: "/lista-grips"
+    },
+    {
+      titulo: "Miércoles",
+      description: "Nov-13-2019",
+      redirectTo: "/lista-grips"
+    },
+    {
+      titulo: "Sábado",
+      description: "Nov-16-2019",
+      redirectTo: "/lista-grips"
+    },
+    {
+      titulo: "Miércoles",
+      description: "Nov-20-2019",
+      redirectTo: "/lista-grips"
+    },
+    {
+      titulo: "Jueves",
+      description: "Nov-21-2019",
+      redirectTo: "/lista-grips"
+    },
+    {
+      titulo: "Domingo",
+      description: "Nov-24-2019",
+      redirectTo: "/lista-grips"
+    },
+    {
+      titulo: "Miércoles",
+      description: "Nov-27-2019",
+      redirectTo: "/lista-grips"
+    },
+    {
+      titulo: "Sábado",
+      description: "Nov-30-2019",
+      redirectTo: "/lista-grips"
+    }
+  ]
+
   @ViewChild(CalendarComponent,{static:false}) myCalendar:CalendarComponent;
   eventSource = [];
 
